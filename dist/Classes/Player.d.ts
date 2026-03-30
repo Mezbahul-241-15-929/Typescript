12aa2f1c29d683c35fccfc91ff11f21da9857ac5
Mezbahul-241-15-929 <islam241-15-929@diu.edu.bd>
2026-03-30 20:07:17 +0600

    Module System

---
+++ dist/Classes/Player.d.ts
@@ -0,0 +1,8 @@
+export declare class Player {
+    name: string;
+    private age;
+    readonly country: string;
+    constructor(name: string, age: number, country: string);
+    play(): void;
+}
+//# sourceMappingURL=Player.d.ts.map
--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,6 @@
-export {};
+import { Player } from "./Classes/Player.js";
+const player1 = new Player("John", 25, "USA");
+const player2 = new Player("Jane", 22, "UK");
+console.log(player1.name); //John
+console.log(player1.country); //USA
 //# sourceMappingURL=script.js.map
